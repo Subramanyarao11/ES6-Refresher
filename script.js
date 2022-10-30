8cc7f869522d747720f349aa71935b9840e92db7
Subramanya M Rao <subramanya11rao@gmail.com>
2022-10-30 08:38:50 +0000

    Array Destruct

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 // Template literals + Object Destructuring
-// const player = {
-//   name: 'Lebron James',
-//   club: 'LA Lakers',
-//   address: {
-//     city: 'Los Angeles'
-//   }
-// };
+const player = {
+  name: 'Lebron James',
+  club: 'LA Lakers',
+  address: {
+    city: 'Los Angeles'
+  }
+};
 
-// const { name,club, address:{ city } } = player;
-// console.log(`${name} plays for ${club} and lives in ${city}`);
+const { name,club, address:{ city } } = player;
+console.log(`${name} plays for ${club} and lives in ${city}`);
 
- 
+
+// Array Destructuring
+let names = ['Subramanya', 'Coding God', 'Rao'];
+const [firstName] = names;
+console.log(firstName , ...names);
+
