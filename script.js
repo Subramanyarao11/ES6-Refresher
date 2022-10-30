b56bba55ee5707c6e3ce5c98ba7d55f5c8e7225f
Subramanya M Rao <subramanya11rao@gmail.com>
2022-10-30 08:22:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Template literals + Object Destructuring
+// const player = {
+//   name: 'Lebron James',
+//   club: 'LA Lakers',
+//   address: {
+//     city: 'Los Angeles'
+//   }
+// };
+
+// const { name,club, address:{ city } } = player;
+// console.log(`${name} plays for ${club} and lives in ${city}`);
+
+ 
