7e3dd19ed1f41ab1e9c7eb152470ec6244ade3c1
Subramanya M Rao <subramanya11rao@gmail.com>
2022-10-30 09:05:26 +0000

    for of loop

--- script.js
+++ script.js
@@ -16,3 +16,35 @@ let names = ['Subramanya', 'Coding God', 'Rao'];
 const [firstName] = names;
 console.log(firstName , ...names);
 
+// Object Literal
+
+function addressMaker({city , state}) {
+    const newAddress = { city , state , country: 'United States'};
+  console.log(newAddress);
+    };
+
+
+addressMaker({city: 'Austin', state: 'Texas'});
+
+// For-of Loop
+
+let incomes = [62000, 67000, 75000];
+let total = 0;
+
+for(const income of incomes)
+  total += income;
+
+console.log(total)
+
+// For of loop Challenge
+
+
+const students = [ 
+    { name: "John", city: "New York" },
+    { name: "Peter", city: "Paris"},
+    { name: "Kate", city: "Sidney" }
+]
+
+for (const student of students){
+    console.log(`${student.name} lives in ${student.city}`);
+}
